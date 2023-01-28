3ffadfba7aeeb987d398c6fd623b5bab676bede0
ptirem <ptirem72@gmail.com>
2023-01-29 00:56:25 +0100

    pas fini implementation du fileexploreur debut e thread

--- src/main.java
+++ src/main.java
@@ -1,33 +1,77 @@
 import java.awt.*;
-import java.awt.event.*;
-
-class ListenerQuitter implements ActionListener
-{ public void actionPerformed(ActionEvent e)
-  { System.exit(0); // arrêter la machine java
-  }
-}
+import java.awt.event.*; 
+import javax.swing.*;
 
 
 
 
-
-class AppliWindow
+class Main extends Thread   
 {
 
-  public static void main(String [] arg) {
-     
+  public static void main(String[] args) {
+    Main thread =new Main();
+    thread.start();
+
+
+    public void run(){
     Frame fen = new Frame ("Bonjour" );
-    fen.setSize(1920,1080);
+    fen.setSize(600,600);
     fen.setLayout(new FlowLayout( ));
     ///////////////////////boutton///////////////////////////////
     
-    Button quitter = new Button("Quitter l'application");
-    Button open_diredctory_explorer = new Button("directory_explorer");
+    Button quitter = new Button("Quitter");
+    Button open_directory_explorer = new Button("directory explorer");
     
     ////////////////////////////////////////////////////////////////
-    quitter.addActionListener(new ListenerQuitter( ));
+    Listener listener=new Listener();
+    quitter.addActionListener(listener);
+    open_directory_explorer.addActionListener(listener);
     fen.add(quitter);
-    fen.add(open_diredctory_explorer);
+    fen.add(open_directory_explorer);
+    fen.show( );
+   }}
+}
+public void run(){
+    Frame fen = new Frame ("Bonjour" );
+    fen.setSize(600,600);
+    fen.setLayout(new FlowLayout( ));
+    ///////////////////////boutton///////////////////////////////
+    
+    Button quitter = new Button("Quitter");
+    Button open_directory_explorer = new Button("directory explorer");
+    
+    ////////////////////////////////////////////////////////////////
+    Listener listener=new Listener();
+    quitter.addActionListener(listener);
+    open_directory_explorer.addActionListener(listener);
+    fen.add(quitter);
+    fen.add(open_directory_explorer);
     fen.show( );
    }
+
+
+
+
+
+
+class Listener implements ActionListener{ 
+    
+    public void actionPerformed(ActionEvent evt){ 
+        Object source=evt.getActionCommand();
+        System.out.println(source);
+        switch((String) source){
+            
+            case "Quitter":System.exit(0);
+            case "directory explorer": {
+                FileTree2.main(null);
+                
+            }
+            
+        }
+    }
 }
+
+    
+    
+
+
